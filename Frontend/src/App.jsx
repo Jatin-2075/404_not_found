2157a6f1b4bdb6f271400f78f252a2c0f9405182
Shivam Gami <shivamprakashgami@gmail.com>
2025-12-13 23:09:41 +0530

    mene kuch kiya hai mat pucho kiya kiya hai

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Layout from "./Components/Layout.jsx";
+import Layout from "./Components/Layout";
 
 import Home from "./Pages/Home";
 import Dashboard from "./Pages/Dashboard";
