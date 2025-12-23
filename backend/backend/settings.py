9b30b63a7c4918185ceda4b0e194afbaf4e6efc3
Shivam Gami <shivamprakashgami@gmail.com>
2025-12-23 19:00:46 +0530

    Update SECRET_KEY configuration in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ import dj_database_url
 # ======================================================
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config("SECRET_KEY_KEY")
+SECRET_KEY = config("SECRET_KEY", default="_v#c)12#_+wfh2$$uoknksjyh)&fkvx@$57oadlyz7&7^j64*3")
 
 
 DEBUG = config("DEBUG", default=False, cast=bool)
